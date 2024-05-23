85cc91b1a7811aa8b0c1050eca66b53fe369d556
srisoundhar <soundhar239@outlook.com>
2024-05-23 07:36:32 +0530

    Repository and relevant data classes implementation

---
+++ app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/data/repository/TopHeadlineRepository.kt
@@ -0,0 +1,21 @@
+package com.sri.soundhar.newsapp_mvvm_architecture.data.repository
+
+import com.sri.soundhar.newsapp_mvvm_architecture.data.api.NetworkService
+import com.sri.soundhar.newsapp_mvvm_architecture.data.model.Article
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TopHeadlineRepository @Inject constructor(private val networkService: NetworkService){
+
+    fun getTopHeadlines(country: String): Flow<List<Article>> {
+        return flow {
+            emit(networkService.getTopHeadlines(country))
+        }.map {
+            it.articles
+        }
+    }
+}
---
+++ app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/uitils/AppConstant.kt
@@ -0,0 +1,7 @@
+package com.sri.soundhar.newsapp_mvvm_architecture.uitils
+
+object AppConstant {
+
+    const val API_KEY = "1cce2d14a7234e4b80a72efefb2f9ff5"
+    const val COUNTRY = "us"
+}
