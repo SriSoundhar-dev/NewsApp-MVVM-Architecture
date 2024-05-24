33887cf343e069803b595335a406443637057ffc
srisoundhar <soundhar239@outlook.com>
2024-05-24 09:01:02 +0530

    UI, recycler adapter, glide integrations

--- app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/di/module/ActivityModule.kt
+++ app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/di/module/ActivityModule.kt
@@ -1,17 +1,18 @@
 package com.sri.soundhar.newsapp_mvvm_architecture.di.module
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 import com.sri.soundhar.newsapp_mvvm_architecture.data.repository.TopHeadlineRepository
 import com.sri.soundhar.newsapp_mvvm_architecture.di.ActivityContext
 import com.sri.soundhar.newsapp_mvvm_architecture.ui.base.ViewModelProviderFactory
+import com.sri.soundhar.newsapp_mvvm_architecture.ui.topheadline.TopHeadlineAdapter
 import com.sri.soundhar.newsapp_mvvm_architecture.ui.topheadline.TopHeadlineViewModel
 import dagger.Module
 import dagger.Provides
 
 @Module
-class ActivityModule(private val activity: AppCompatActivity) {
+class ActivityModule(private val activity: ComponentActivity) {
 
     @ActivityContext
     @Provides
@@ -26,4 +27,7 @@ class ActivityModule(private val activity: AppCompatActivity) {
                 TopHeadlineViewModel(topHeadlineRepository)
             })[TopHeadlineViewModel::class.java]
     }
+
+    @Provides
+    fun provideTopHeadlineAdapter() = TopHeadlineAdapter(ArrayList())
 }
--- app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/ui/topheadline/TopHeadlineActivity.kt
+++ app/src/main/java/com/sri/soundhar/newsapp_mvvm_architecture/ui/topheadline/TopHeadlineActivity.kt
@@ -10,8 +10,12 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sri.soundhar.newsapp_mvvm_architecture.NewsApplication
 import com.sri.soundhar.newsapp_mvvm_architecture.R
+import com.sri.soundhar.newsapp_mvvm_architecture.data.model.Article
+import com.sri.soundhar.newsapp_mvvm_architecture.databinding.ActivityTopHeadlineBinding
 import com.sri.soundhar.newsapp_mvvm_architecture.di.component.DaggerActivityComponent
 import com.sri.soundhar.newsapp_mvvm_architecture.di.module.ActivityModule
 import com.sri.soundhar.newsapp_mvvm_architecture.ui.base.UiState
@@ -22,37 +26,49 @@ class TopHeadlineActivity : AppCompatActivity() {
 
     @Inject
     lateinit var newsListViewModel: TopHeadlineViewModel
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        injectDependencies()
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_top_headline)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
+    @Inject
+    lateinit var adapter: TopHeadlineAdapter
+
+    private lateinit var binding: ActivityTopHeadlineBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        injectDependencies()
+        super.onCreate(savedInstanceState)
+        binding = ActivityTopHeadlineBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUI()
         setupObserver()
+
     }
 
+    private fun setupUI() {
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
     private fun setupObserver() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 newsListViewModel.uiState.collect {
                     when (it) {
                         is UiState.Success -> {
-                            //binding.progressBar.visibility = View.GONE
-                            //renderList(it.data)
-                            //binding.recyclerView.visibility = View.VISIBLE
+                            binding.progressBar.visibility = View.GONE
+                            renderList(it.data)
+                            binding.recyclerView.visibility = View.VISIBLE
                         }
                         is UiState.Loading -> {
-                            //binding.progressBar.visibility = View.VISIBLE
-                            //binding.recyclerView.visibility = View.GONE
+                            binding.progressBar.visibility = View.VISIBLE
+                            binding.recyclerView.visibility = View.GONE
                         }
                         is UiState.Error -> {
                             //Handle Error
-                            //binding.progressBar.visibility = View.GONE
+                            binding.progressBar.visibility = View.GONE
                             Toast.makeText(this@TopHeadlineActivity, it.message, Toast.LENGTH_LONG)
                                 .show()
                         }
@@ -61,6 +77,11 @@ class TopHeadlineActivity : AppCompatActivity() {
             }
         }
     }
+    private fun renderList(articleList: List<Article>) {
+        adapter.addData(articleList)
+        adapter.notifyDataSetChanged()
+    }
+
     private fun injectDependencies() {
         DaggerActivityComponent.builder()
             .applicationComponent((application as NewsApplication).applicationComponent)
